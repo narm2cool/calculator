5b629ffd96e5b35d8ec5a6a8ae0b0c809ae8dcf9
unknown <narmadhamayilraj@gmail.com>
2022-08-25 19:55:01 +0530

    fierst

---
+++ Mathematics.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public interface Mathematics {
+
+
+
+	int multiplication(int num1, int num2);
+	int subtraction(int num1, int num2);
+	int addition(int num1, int num2);
+	int division(int num1, int num2);
+
+}
